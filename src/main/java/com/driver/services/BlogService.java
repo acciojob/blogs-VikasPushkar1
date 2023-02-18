f2b501ac85d642fa84509c72e65634be07aaffd7
VikasPushkar1 <vikaspushkar9354@gmail.com>
2023-02-18 22:16:15 +0530

    Blog

--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -23,11 +23,21 @@ public class BlogService {
 
     public Blog createAndReturnBlog(Integer userId, String title, String content) {
         //create a blog at the current time
+        Blog blog=new Blog();
+        blog.setTitle(title);
+        blog.setContent(content);
+        blog.setPubDate(new Date());
 
+        User user= userRepository1.findById(userId).get();
+        user.getBlogList().add(blog);
+
+        userRepository1.save(user);    //by cascading effect child(blog) will be save
+        return blog;
     }
 
     public void deleteBlog(int blogId){
         //delete blog and corresponding images
+        blogRepository1.deleteById(blogId);
 
     }
 }
--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -17,15 +17,51 @@ public class ImageService {
 
     public Image addImage(Integer blogId, String description, String dimensions){
         //add an image to the blog
+        Image image=new Image();
+        image.setDescription(description);
+        image.setDescription(dimensions);
+
+
+        Blog blog=blogRepository2.findById(blogId).get();
+        blog.getImageList().add(image);
+
+
+        blogRepository2.save(blog);
+
+
+
+        return image;
+
 
     }
 
     public void deleteImage(Integer id){
+        imageRepository2.deleteById(id);
 
     }
 
     public int countImagesInScreen(Integer id, String screenDimensions) {
         //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
+        Image image=imageRepository2.findById(id).get();
+        String dimension=image.getDimensions();
+
+        String []a=screenDimensions.split("X");
+        String []b=dimension.split("X");
+
+        int l1=Integer.parseInt(a[0]);
+        int b1=Integer.parseInt(a[1]);
+
+        int l2=Integer.parseInt(b[0]);
+        int b2=Integer.parseInt(b[1]);
+
+        int length=l1/l2;
+        int breadth=b1/b2;
+
+        return length*breadth;
+
+
+
+
 
     }
 }
